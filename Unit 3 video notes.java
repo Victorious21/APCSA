ceeed8a3976326d3ccfcd1401e975d7b2b170c53
Victorious21 <42615974+Victorious21@users.noreply.github.com>
2020-10-12 07:10:40 -0400

    Create Unit 3 video notes.java

---
+++ Unit 3 video notes.java
@@ -0,0 +1,27 @@
+//Boolean expressions:
+== is equals to
+!=  not equals to
+< less than 
+> greater than
+|| rational operators
+//operators^^
+-------------------------
+int myAge = 17;
+int jwAge = 23;
+
+myAge < 16 - false
+myAge != jwAge - true
+myAge == 15 - false
+myAge > jwAge - false
+myAge <= 15 - false
+//testing the boolean^^
+
+boolean enrollment = APCSA;
+enrollment = true;
+enrollment != false;
+//both mean the same thing^^
+
+String myName = Victor;
+myName !=Jadon;
+myName == Jessica;
+//better to use .equal or .compareTo since you can get false positives^^
